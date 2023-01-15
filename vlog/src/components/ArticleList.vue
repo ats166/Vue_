ebfff4ca33cac2d28aa9e4b2a449244cb4dc0292
ats166 <45484403+ats166@users.noreply.github.com>
2023-01-16 00:47:59 +0900

    ca_vuestagram

--- vlog/src/components/ArticleList.vue
+++ vlog/src/components/ArticleList.vue
@@ -1,19 +1,9 @@
 <template>
   <div>
-    <div>
-      <router-link to="/detail/0" >{{ data[0].title }}</router-link>
-      <p>{{ data[0].content }}</p>
-      <h6 class="mb-5">{{ data[0].date }}</h6>
-    </div>
-    <div>
-      <router-link to="/detail/1" >{{ data[1].title }}</router-link>
-      <p>{{ data[1].content }}</p>
-      <h6 class="mb-5">{{ data[1].date }}</h6>
-    </div>
-    <div>
-      <router-link to="/detail/2" >{{ data[2].title }}</router-link>
-      <p>{{ data[2].content }}</p>
-      <h6 class="mb-5">{{ data[2].date }}</h6>
+    <div v-for="(dat,i) in data" :key = "i">
+      <h5 @click="$router.push(`/detail/${i}`)">{{ dat.title }}</h5>
+      <p>{{ dat.content }}</p>
+      <h6 class="mb-5">{{ dat.date }}</h6>
     </div>
   </div>
 </template>
