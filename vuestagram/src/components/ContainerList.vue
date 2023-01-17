f45f8f4790a50ec2c7cc17989c68f26f8c7cda4d
ats166 <45484403+ats166@users.noreply.github.com>
2023-01-17 21:57:43 +0900

    ca_vue

---
+++ vuestagram/src/components/ContainerList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <PostVue v-for="(dat,i) in data" :key="i"
+      :data = dat
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import PostVue from './PostVue'
+
+export default {
+  name:"ContainerList",
+  components : {
+    PostVue
+  },
+  props : {
+    data : Array,
+    step : Number
+  }
+
+};
+</script>
+
+<style>
+</style>
